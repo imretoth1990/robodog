6703f36f2181266afa6745abf8d2c2ccc71cf3bf
Imre Toth <imretoth1990@gmail.com>
2023-04-26 20:18:39 +0200

    Add existing robodog project

---
+++ src/main/java/com/imretoth/robodog/RobodogApplication.java
@@ -0,0 +1,13 @@
+package com.imretoth.robodog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobodogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobodogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imretoth/robodog/RobodogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imretoth.robodog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobodogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
